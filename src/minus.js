42969b031570fe8e38ee85a4246a4776b35c9f3d
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-04-06 16:02:05 +0530

    minus function is added

--- src/minus.js
+++ src/minus.js
@@ -1,3 +1,15 @@
-export const minus = () => {
-    
+export const minus = (arr = []) => {
+    if (arr.length === 0) return;
+
+    let result = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        const elem = arr[i];
+
+        result = (() => {
+            return result > elem ? result - elem : elem - result;
+        })();
+    }
+
+    console.log(`Result: ${result}`);
 }
